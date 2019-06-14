1fba2478d9136fb83a69db13c82aea7608e853f2
daniel <daniel.stolzberg@gmail.com>
2019-06-14 08:35:25 -0400

    Helper object includes events to notify for important events

---
+++ obj/+epsych/@Helper/Helper.m
@@ -0,0 +1,17 @@
+classdef Helper < handle
+    
+
+    properties
+
+    end
+
+    events (ListenAccess = 'public', NotifyAccess = 'public')
+        NewData
+        NewTrial
+    end
+
+    methods
+
+    end
+
+end
