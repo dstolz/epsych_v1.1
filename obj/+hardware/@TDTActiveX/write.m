68e84c8c97d1b6027321faa3416afbf0c73f0d28
daniel <daniel.stolzberg@gmail.com>
2020-03-28 11:41:58 -0400

    untested additions; needs work

--- obj/+hardware/@TDTActiveX/write.m
+++ obj/+hardware/@TDTActiveX/write.m
@@ -8,14 +8,25 @@ end
     
 mind = parameter.ModuleID == obj.ModuleID;
 
-switch parameter.DataClass
-    case 'scalar'
-        e = obj.handle(mind).SetTagVal(parameter.Name,value);
 
-    case 'buffer'
-        value = value(:)';
-        e = obj.handle(mind).WriteTagV(parameter.Name,value);
+if isa(parameter,'parameter.Parameter')
 
-    case 'table'
-        e = obj.handle(mind).WriteTagVEX(parameter.Name,0,'F32',value);
+    switch parameter.DataClass
+        case 'scalar'
+            e = obj.handle(mind).SetTagVal(parameter.Name,value);
+
+        case 'buffer'
+            value = value(:)';
+            e = obj.handle(mind).WriteTagV(parameter.Name,value);
+
+        case 'table'
+            e = obj.handle(mind).WriteTagVEX(parameter.Name,0,'F32',value);
+    end
+
+elseif isa(parameter,'parameter.Group')
+    e = arrayfun(@write,parameter.Parameters);
+
+else
+    p = parameter.Parameter(parameter);
+    e = write(p,value);
 end
