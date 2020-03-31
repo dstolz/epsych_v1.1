271db1d3671b1f5863e18f40334b951f80ff36f1
daniel <daniel.stolzberg@gmail.com>
2020-03-31 12:20:01 -0400

    added toolbar

--- obj/+epsych/+ui/@ControlPanel/create.m
+++ obj/+epsych/+ui/@ControlPanel/create.m
@@ -21,12 +21,13 @@ if nargin == 1 || isempty(parent)
 end
 
 g = uigridlayout(obj.parent);
-g.RowHeight   = {'.3x','.6x',22};
+g.RowHeight   = {30,'.3x','.6x',25};
 g.ColumnWidth = {'0.8x','0.2x'};
+
 
 % Create TabGroup
 obj.TabGroup = uitabgroup(g);
-obj.TabGroup.Layout.Row    = [1 2];
+obj.TabGroup.Layout.Row    = [2 3];
 obj.TabGroup.Layout.Column = 1;
 
 % Create SubjectTab
@@ -52,39 +53,39 @@ RUNTIME.Log.create_gui(obj.LogTab);
 
 
 
-% Create Load/Save Config Panel
-obj.ConfigPanel = uipanel(g);
-obj.ConfigPanel.Layout.Row = 1;
-obj.ConfigPanel.Layout.Column = 2;
-obj.ConfigPanel.Title = 'Config';
+% Create "Toolbar"
+obj.ToolbarPanel = uipanel(g);
+obj.ToolbarPanel.Layout.Row = 1;
+obj.ToolbarPanel.Layout.Column = 1;
+obj.ToolbarPanel.BorderType = 'none';
 
-gc = uigridlayout(obj.ConfigPanel);
-gc.RowHeight = {30,30};
-gc.ColumnWidth = {'1x',70,'1x'};
+gc = uigridlayout(obj.ToolbarPanel);
+gc.Padding = [0 0 0 0];
+gc.RowHeight = {'1x'};
+gc.ColumnWidth = {50,50};
 
 h = uibutton(gc);
-h.Layout.Row = 1;
-h.Layout.Column = 2;
 h.Text = 'load';
-h.Icon = fullfile(epsych.Info.root,'icons','folder.png');
+% h.Icon = fullfile(epsych.Info.root,'icons','folder.png');
 h.ButtonPushedFcn = @obj.load_config;
 
 h = uibutton(gc);
-h.Layout.Row = 2;
-h.Layout.Column = 2;
 h.Text = 'save';
-h.Icon = fullfile(epsych.Info.root,'icons','save.png');
+% h.Icon = fullfile(epsych.Info.root,'icons','save.png');
 h.ButtonPushedFcn = @obj.save_config;
 
 
 
 % Create RuntimePanel
 obj.RuntimePanel = uipanel(g);
-obj.RuntimePanel.Layout.Row = 2;
+obj.RuntimePanel.Layout.Row = [2 3];
 obj.RuntimePanel.Layout.Column = 2;
 obj.RuntimeControlObj = epsych.ui.RuntimeControl(obj.RuntimePanel,'vertical');
+
+
+
 
 % Create AlwaysOnTop
 obj.AlwaysOnTopCheckbox = epsych.ui.FigOnTop(g,0,'epsych_ControlPanel');
 obj.AlwaysOnTopCheckbox.handle.Layout.Column = 2;
-obj.AlwaysOnTopCheckbox.handle.Layout.Row    = 3;
+obj.AlwaysOnTopCheckbox.handle.Layout.Row    = 4;
