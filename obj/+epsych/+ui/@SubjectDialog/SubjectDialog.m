0f8bcb2da216fdfd0fec29d9606c3f52ca2b4e3b
daniel <daniel.stolzberg@gmail.com>
2020-04-02 11:02:42 -0400

    formatting and features

--- obj/+epsych/+ui/@SubjectDialog/SubjectDialog.m
+++ obj/+epsych/+ui/@SubjectDialog/SubjectDialog.m
@@ -1,18 +1,31 @@
 classdef SubjectDialog < handle
+    % h = epsych.ui.SubjectDialog([Subject],[parent]);
+    %
+    % Use the following to block execution:
+    %   waitfor(h.parent);
+    %   disp(h.UserResponse)   % can equal 'OK' or 'Cancel'
+    %
+    % Subject data is in h.Subject
+
     properties
         Subject    (1,1) epsych.Subject = epsych.Subject;
+    end
 
-
-        NameEditField   (1,1) matlab.ui.control.EditField
-        IDEditField     (1,1) matlab.ui.control.EditField
-        DOBDatePicker   (1,1) matlab.ui.control.DatePicker
-        SexDropDown     (1,1) matlab.ui.control.DropDown
-        BaselineWeightEditField     (1,1) matlab.ui.control.NumericEditField
-        NoteTextArea    (1,1) matlab.ui.control.TextArea
-        
+    properties (Access = protected)
+        NameEditField           (1,1) matlab.ui.control.EditField
+        IDEditField             (1,1) matlab.ui.control.EditField
+        DOBDatePicker           (1,1) matlab.ui.control.DatePicker
+        SexDropDown             (1,1) matlab.ui.control.DropDown
+        BaselineWeightEditField (1,1) matlab.ui.control.NumericEditField
+        ProtocolFileEditField   (1,1) matlab.ui.control.EditField
+        LocateProtocolButton    (1,1) matlab.ui.control.Button
+        NoteTextArea            (1,1) matlab.ui.control.TextArea
+        OKButton                (1,1) matlab.ui.control.Button
+        CancelButton            (1,1) matlab.ui.control.Button
     end
 
     properties (SetAccess = private)
+        UserResponse    (1,:) char = 'Cancel'; % default if closed manually
         parent
     end
 
@@ -25,6 +38,8 @@ classdef SubjectDialog < handle
 
             obj.create(parent);
 
+            
+            epsych.ui.FigOnTop.figure_state(obj.parent,true);
         end
 
         function create_field(obj,hObj,event)
@@ -44,12 +59,24 @@ classdef SubjectDialog < handle
             end
         end
 
-        function save_subject(obj,hObj,event)
+        function response_button(obj,hObj,event)
+            obj.UserResponse = hObj.Tag;
+            close(obj.parent);
+        end
+
+    end % methods (Access = public)
+
+    methods (Access = private)
+        function locate_file(obj,hObj,event)
+            pn = getpref('epsych',[hObj.Tag 'Path'],epsych.Info.user_directory);
+
+            [fn,pn] = uigetfile(hObj.UserData{:},pn);
+
+            if isequal(fn,0), return; end
+
+            setpref('epsych',[hObj.Tag 'Path'],pn);
             
+            obj.Subject.(hObj.Tag) = fullfile(pn,fn);
         end
-
-        function load_subject(obj,hObj,event)
-
-        end
-    end
+    end % methods (Access = private)
 end
--- obj/+epsych/+ui/@SubjectDialog/create.m
+++ obj/+epsych/+ui/@SubjectDialog/create.m
@@ -5,14 +5,15 @@ if isempty(parent)
     % Create parent
     parent = uifigure(parent);
     parent.Name = 'Subject Info';
-    parent.Position([3 4]) = [350 300];
+    parent.Position([3 4]) = [350 350];
+    parent.Scrollable = 'on';
 end
 
 obj.parent = parent;
 
 g = uigridlayout(parent);
-g.ColumnWidth = {100,'1x'};
-g.RowHeight   = {25,25,25,25,25,25,'1x',30};
+g.ColumnWidth = {100,'1x',25};
+g.RowHeight   = {25,25,25,25,25,25,70,'1x',30};
 
 R = 1;
 
@@ -61,6 +62,7 @@ h.Layout.Column = 1;
 obj.SexDropDown = uidropdown(g,'Tag','Sex','Items',{'male','female','unknown'},'CreateFcn',@obj.create_field);
 obj.SexDropDown.Layout.Row = R;
 obj.SexDropDown.Layout.Column = 2;
+obj.SexDropDown.Value = 'unknown';
 obj.SexDropDown.ValueChangedFcn = @obj.update_field;
 
 
@@ -83,35 +85,53 @@ obj.BaselineWeightEditField.ValueChangedFcn = @obj.update_field;
 
 R = R + 1;
 
-h = uilabel(g,'Text','Note:');
+h = uilabel(g,'Text','Protocol File:');
 h.Layout.Row = R;
 h.Layout.Column = 1;
+
+obj.ProtocolFileEditField = uieditfield(g,'Tag','ProtocolFile','CreateFcn',@obj.create_field);
+obj.ProtocolFileEditField.Layout.Row = R;
+obj.ProtocolFileEditField.Layout.Column = 2;
+obj.ProtocolFileEditField.ValueChangedFcn = @obj.update_field;
+obj.ProtocolFileEditField.HorizontalAlignment = 'right';
+
+obj.LocateProtocolButton = uibutton(g,'Tag','ProtocolFile');
+obj.LocateProtocolButton.Layout.Row = R;
+obj.LocateProtocolButton.Layout.Column = 3;
+obj.LocateProtocolButton.Text = '...';
+obj.LocateProtocolButton.ButtonPushedFcn = @obj.locate_file;
+obj.LocateProtocolButton.UserData = {{'*.epprot','EPsych Protocol'},'EPsych Protocol File'}; % uigetfile parameters
+
+
+
 
 R = R + 1;
 
 obj.NoteTextArea = uitextarea(g,'Tag','Note','CreateFcn',@obj.create_field);
 obj.NoteTextArea.Layout.Row = R;
 obj.NoteTextArea.Layout.Column = [1 2];
+obj.NoteTextArea.Value = '< Notes >';
 obj.NoteTextArea.ValueChangedFcn = @obj.update_field;
 
 
 
-R = R + 1;
+
+R = length(g.RowHeight);
 
 p = uipanel(g);
 p.Layout.Row = R;
 p.Layout.Column = [1 2];
 p.BorderType = 'none';
 
-h = uibutton(p);
-h.Position = [100 2 50 25];
-h.Text = 'Save';
-h.ButtonPushedFcn = @obj.save_subject;
+obj.OKButton = uibutton(p,'Tag','OK');
+obj.OKButton.Position = [100 2 50 25];
+obj.OKButton.Text = 'OK';
+obj.OKButton.ButtonPushedFcn = @obj.response_button;
 
-h = uibutton(p);
-h.Position = [160 2 50 25];
-h.Text = 'Load';
-h.ButtonPushedFcn = @obj.load_subject;
+obj.CancelButton = uibutton(p,'Tag','Cancel');
+obj.CancelButton.Position = [160 2 50 25];
+obj.CancelButton.Text = 'Cancel';
+obj.CancelButton.ButtonPushedFcn = @obj.response_button;
 
 
 
