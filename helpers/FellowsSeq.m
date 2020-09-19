b03988ea00d6c5d90ac96f0dffe5a5ab4cbe6792
daniel <daniel.stolzberg@gmail.com>
2020-09-19 13:33:54 -0400

    Fellows trial sequences

---
+++ helpers/FellowsSeq.m
@@ -0,0 +1,45 @@
+function [S,M,cnt] = FellowsSeq(nSeq)
+% S = FellowsSeq([nSeq])
+% [S,M,cnt] = FellowsSeq([nSeq])
+%
+% Generates sequences described in: Fellows, B. CHANCE STIMULUS SEQUENCES FOR
+% DISCRIMINATION TASKS, Psychological Bulletin, 1967, Vol. 67, No. 2, 87-92
+%
+% default nSeq = 100
+%
+% DJS 2020
+
+if nargin < 1 || isempty(nSeq), nSeq = 100; end
+
+M = uint8([0 1 0 0 1 1 1 0 0 0 1 1;1 0 1 1 0 0 0 1 1 1 0 0;0 0 1 1 1 0 0 0 1 1 0 1;1 1 0 0 0 1 1 1 0 0 1 0;0 1 1 0 0 0 1 1 1 0 0 1;1 0 0 1 1 1 0 0 0 1 1 0;0 0 1 0 0 0 1 1 0 1 1 1;1 1 0 1 1 1 0 0 1 0 0 0;0 0 0 1 0 0 1 1 1 0 1 1;1 1 1 0 1 1 0 0 0 1 0 0;0 1 1 1 0 0 1 0 0 0 1 1;1 0 0 0 1 1 0 1 1 1 0 0;0 1 1 0 0 0 1 0 0 1 1 1;1 0 0 1 1 1 0 1 1 0 0 0;0 0 1 1 1 0 1 1 0 0 0 1;1 1 0 0 0 1 0 0 1 1 1 0;0 0 0 1 1 0 1 1 1 0 0 1;1 1 1 0 0 1 0 0 0 1 1 0;0 0 1 0 1 1 0 0 0 1 1 1;1 1 0 1 0 0 1 1 1 0 0 0;0 0 0 1 1 0 1 0 0 1 1 1;1 1 1 0 0 1 0 1 1 0 0 0;0 0 0 1 1 1 0 0 1 0 1 1;1 1 1 0 0 0 1 1 0 1 0 0]);
+
+n = size(M,1);
+
+cnt = zeros(n,1);
+
+idx = randi(n,1);
+
+S = zeros(1,12*nSeq,'uint8');
+
+k = 1;
+for i = 1:nSeq
+    a = M(idx,end-1:end);
+    b = M(:,1:2);
+    
+    if a(1) == a(2)
+        idx = all(b~=a,2);
+    else
+        idx = b(:,1) == a(2) & b(:,2) == a(1) & b(:,1) ~= b(:,2);
+    end
+    idx = find(idx);
+    
+    x = find(cnt(idx)==min(cnt(idx)));
+    idx = idx(randi(length(x),1));
+    
+    cnt(idx) = cnt(idx) + 1;
+    
+    S(k:k+11) = M(idx,:);
+    
+    k = k + 12;
+end
+
