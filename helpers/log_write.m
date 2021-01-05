6ddb4f21550275f0e86a352bdb28296ae03b54c5
dstolz <daniel.stolzberg@gmail.com>
2021-01-05 12:34:57 -0500

    performance note

--- helpers/log_write.m
+++ helpers/log_write.m
@@ -2,6 +2,10 @@ function log_write(varargin)
 % log_write(...)
 %
 % Convenience function for epsych.log.Log.write.  Uses global RUNTIME.Log.
+%
+% Note that this function has some additional overhead becuase it uses 
+% 'global RUNTIME' on each call. For more time-critical code, use the 
+% log object directly.
 
 
 global RUNTIME
