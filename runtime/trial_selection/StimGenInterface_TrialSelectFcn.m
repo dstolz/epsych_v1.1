242df21425ae1a11758bec5f9a50ec2b3c630fcf
daniel <daniel.stolzberg@gmail.com>
2021-09-04 17:48:27 -0400

    dummy trialselect function

---
+++ runtime/trial_selection/StimGenInterface_TrialSelectFcn.m
@@ -0,0 +1,22 @@
+function TRIALS = StimGenInterface_TrialSelectFcn(TRIALS)
+% TRIALS = StimGenInterface_TrialSelectFcn(TRIALS)
+% 
+% Dummy function
+%
+% 
+% Daniel.Stolzberg@gmail.com 2021
+
+% updated DJS 3/15/2019
+
+
+
+
+TRIALS.NextTrialID = 1;
+
+
+
+
+
+
+
+
