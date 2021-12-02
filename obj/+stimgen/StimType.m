7c11e3719efdd93aadf933a4bd2659867a3f719c
caraslab <48964621+caraslab@users.noreply.github.com>
2021-12-02 14:42:42 -0500

    changed how to compute Time vector

--- obj/+stimgen/StimType.m
+++ obj/+stimgen/StimType.m
@@ -51,7 +51,7 @@ classdef (Hidden) StimType < handle & matlab.mixin.Heterogeneous & matlab.mixin.
         end
         
         function t = get.Time(obj)
-            t = linspace(0,obj.Duration-1./obj.Fs,obj.N);
+            t = (0:length(obj.Signal)-1)./obj.Fs;
         end
         
         function n = get.N(obj)
