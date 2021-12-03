7d3797da87f353f8ab03ef44bc02748b27467039
caraslab <48964621+caraslab@users.noreply.github.com>
2021-12-03 11:03:51 -0500

    restored original time calculation

--- obj/+stimgen/StimType.m
+++ obj/+stimgen/StimType.m
@@ -59,7 +59,7 @@ classdef (Hidden) StimType < handle & matlab.mixin.Heterogeneous & matlab.mixin.
         end
         
         function t = get.Time(obj)
-            t = (0:length(obj.Signal)-1)./obj.Fs;
+            t = linspace(0,obj.Duration-1./obj.Fs,obj.N);
         end
         
         function n = get.N(obj)
