1015e5e332c6746716129a38481f3599e442b032
Daniel <daniel.stolzberg@gmail.com>
2023-08-10 16:14:06 -0400

    Integrated Melissa's Synapase code

--- runtime/helpers/TrigDATrial.m
+++ runtime/helpers/TrigDATrial.m
@@ -18,7 +18,45 @@ t = clock; %DJS 6/2015
 if ~e, throwerrormsg(trig); end
 pause(0.001)
 e = DA.SetTargetVal(trig,0); 
-if ~e, throwerrormsg(trig); end
+
+%If open developer controls don't work, try SYNAPSE controls
+if ~e
+    fprintf('Trying to trigger with synapse...')
+    
+    global SYN %#ok<TLEV>
+    
+   %Find the RZ6 amongst the gizmos
+    gizmo_names = SYN.getGizmoNames();
+    
+    for i = 1:numel(gizmo_names)
+        name = gizmo_names{i};
+        modInfo = SYN.getGizmoInfo(name);
+        switch modInfo.cat
+            case {'Legacy'}%RZ6
+                break
+        end
+        
+    end
+    
+    RZ6 = gizmo_names{i};
+    
+    %Rename the trigger so it's compatible with the SYNAPSE API syntax
+    trig = trig(strfind(trig,'.')+1:end);
+
+    %Set and reset the trigger value here
+    e = SYN.setParameterValue(RZ6,trig,1);
+    t = clock;
+    pause(0.001)
+    e = SYN.setParameterValue(RZ6,trig,0);
+  
+end
+
+%If synapse controls also don't work, throw an error
+if ~e
+    throwerrormsg(trig);
+end
+
+
 
 function throwerrormsg(trig)
 beep
